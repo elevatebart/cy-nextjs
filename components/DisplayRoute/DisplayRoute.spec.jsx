6e2a1f6e140fce5d5c9605f903f9e902da56788b
ElevateBart <ledouxb@gmail.com>
2021-04-16 10:52:09 -0500

    make it work ;)

--- components/DisplayRoute/DisplayRoute.spec.jsx
+++ components/DisplayRoute/DisplayRoute.spec.jsx
@@ -2,9 +2,11 @@
 import * as React from "react";
 import { mount } from "@cypress/react";
 import DisplayRoute from "./DisplayRoute";
+import * as nextRouter from "next/router";
 
 describe("<DisplayRoute />", () => {
   it("Renders component", () => {
+    cy.stub(nextRouter, "useRouter").callsFake(() => ({ pathname: "coco" }));
     mount(<DisplayRoute />);
 
     cy.get("p").should("contain", "coco");
