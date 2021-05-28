29168f78c62a0ba087dbd397eed494d9baff8d41
ElevateBart <ledouxb@gmail.com>
2021-05-28 18:02:50 -0500

    reploduce the stub es6 issue

--- components/DisplayRoute/DisplayRoute.jsx
+++ components/DisplayRoute/DisplayRoute.jsx
@@ -1,6 +1,13 @@
 import { useRouter } from "next/router";
+import { test } from "../../test";
 
 export default () => {
   const router = useRouter();
-  return <p>{router.pathname}</p>;
+
+  return (
+    <p>
+      {router.pathname}
+      {test()}
+    </p>
+  );
 };
--- components/DisplayRoute/DisplayRoute.spec.jsx
+++ components/DisplayRoute/DisplayRoute.spec.jsx
@@ -3,12 +3,21 @@ import * as React from "react";
 import { mount } from "@cypress/react";
 import DisplayRoute from "./DisplayRoute";
 import * as nextRouter from "next/router";
+import * as utils from "../../test";
 
 describe("<DisplayRoute />", () => {
   it("Renders component", () => {
     cy.stub(nextRouter, "useRouter").callsFake(() => ({ pathname: "coco" }));
     mount(<DisplayRoute />);
 
-    cy.get("p").should("contain", "coco");
+    cy.get("p").should("contain", "cocostory");
+  });
+
+  it("mocks test", () => {
+    cy.stub(utils, "test").callsFake(() => "story");
+    cy.stub(nextRouter, "useRouter").callsFake(() => ({ pathname: "coco" }));
+    mount(<DisplayRoute />);
+
+    cy.get("p").should("contain", "cocostory");
   });
 });
